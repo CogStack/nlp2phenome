bd3029bae7a7e89c96d6764ca565d1e09f98324c
Honghan <honghan.wu@gmail.com>
2019-09-06 21:54:54 +0100

    use direct nlp results as prediction (cui to phenotype mapping can be manually defined)

--- predict_helper.py
+++ predict_helper.py
@@ -1,8 +1,11 @@
-from nlp_to_phenome import LabelModel, LabelPerformance, StrokeSettings, Concept2Mapping, escape_lable_to_filename
+from nlp_to_phenome import LabelModel, LabelPerformance, StrokeSettings, Concept2Mapping, escape_lable_to_filename, \
+    CustomisedRecoginiser, PhenotypeAnn
 import utils
 import logging
 from os.path import join
 from ann_converter import AnnConverter
+from os import listdir
+from os.path import isfile
 
 
 def predict(settings):
@@ -48,6 +51,39 @@ def predict(settings):
     return doc2predicted
 
 
+def direct_nlp_prediction(settings):
+    ann_dir = settings['test_ann_dir']
+    test_text_dir = settings['test_fulltext_dir']
+    _concept_mapping = settings['concept_mapping_file']
+    _learning_model_dir = settings['learning_model_dir']
+    _labels = utils.read_text_file(settings['entity_types_file'])
+    ignore_mappings = utils.load_json_data(settings['ignore_mapping_file'])
+    _cm_obj = Concept2Mapping(_concept_mapping)
+    file_keys = [f.split('.')[0] for f in listdir(ann_dir) if isfile(join(ann_dir, f))]
+    doc2predicted = {}
+    for fk in file_keys:
+        cr = CustomisedRecoginiser(join(ann_dir, '%s.json' % fk), _concept_mapping)
+        d = cr.full_text_file_pattern % fk
+        for ann in cr.annotations:
+            if ann.cui in _concept_mapping.cui2label:
+                lbl = _concept_mapping.cui2label[ann.cui]
+                pheAnn = PhenotypeAnn(ann.str, ann.start, ann.end, ann.negation, ann.temporality, ann.experiencer,
+                                      'StudyName', lbl)
+                put_ann_label(lbl, pheAnn, doc2predicted, d)
+        for ann in cr.phenotypes:
+            put_ann_label(ann.minor_type, ann, doc2predicted, d)
+    return doc2predicted
+
+
+def put_ann_label(lbl, pheAnn, doc2predicted, d):
+    labeled_ann = {'label': lbl,
+                   'ann': pheAnn}
+    if d not in doc2predicted:
+        doc2predicted[d] = [labeled_ann]
+    else:
+        doc2predicted[d].append(labeled_ann)
+
+
 def output_eHOST_format(doc2precited, output_folder):
     for d in doc2precited:
         xml = AnnConverter.to_eHOST(d, doc2precited[d])
@@ -56,8 +92,13 @@ def output_eHOST_format(doc2precited, output_folder):
 
 def predict_to_eHOST_results(predict_setting):
     ss = StrokeSettings(predict_setting)
-    logging.info('predicting...')
-    output_eHOST_format(predict(ss.settings), ss.settings['output_folder'])
+    if 'predict_mode' in ss.settings and ss.settings['predict_mode'] == 'direct_nlp':
+        logging.info('predicting with direct nlp...')
+        predicted_results = direct_nlp_prediction(ss.settings)
+    else:
+        logging.info('predicting...')
+        predicted_results = predict(ss.settings)
+    output_eHOST_format(predicted_results, ss.settings['output_folder'])
     logging.info('results saved to %s' % ss.settings['output_folder'])
 
 
